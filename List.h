bddd28bbeab5bab72b7e94baed04e38d403250e7
Denis Tikhoglo <redneloss@gmail.com>
2015-06-13 22:22:15 +0300

    uploaded files

---
+++ List.h
@@ -0,0 +1,310 @@
+#ifndef LIST
+#define LIST
+
+template<typename T>
+class List
+{
+public:
+	struct Node
+	{
+		Node(T);
+		T m_data;
+		Node* m_pNext;
+		Node* m_pPrev;
+	};
+	List();
+	~List();
+	void push_front(const T&);
+	void push_back(const T&);
+	void pop_front();
+	void pop_back();
+	T& front();
+	T& back();
+	class iterator
+	{
+	public:
+		iterator(Node*&);
+		T& operator*();
+		iterator& operator++();
+		iterator operator++(int);
+		iterator& operator--();
+		iterator operator--(int);
+		bool operator != (const iterator&);
+		Node* m_ptr;
+	};
+	iterator begin();
+	iterator end();
+	void insert(iterator, const T&);
+	void erase(iterator);
+	void clear();
+	bool empty();
+
+private:
+	int m_size;
+	Node* m_pHead;
+	Node* m_pTail;
+};
+
+template<typename T>
+List<T>::Node::Node(T data)
+{
+	m_data = data;
+	m_pNext = m_pPrev = nullptr;
+}
+
+template<typename T>
+List<T>::List()
+{
+	m_size = 0;
+	m_pHead = m_pTail = nullptr;
+}
+
+template<typename T>
+List<T>::~List()
+{
+	if (!empty())
+	{
+		Node* i = m_pHead;
+		while (true)
+		{
+			i = i->m_pNext;
+			if (i != nullptr)
+			{
+				delete i->m_pPrev;
+				i->m_pPrev = nullptr;
+			}
+			else
+			{
+				delete m_pTail;
+				m_pTail = m_pHead = nullptr;
+				break;
+			}
+		}
+		m_size = 0;
+	}
+}
+
+template<typename T>
+void List<T>::push_front(const T& value)
+{
+	if (m_pHead == nullptr)
+	{
+		m_pHead = new Node(value);
+		m_pTail = m_pHead;
+	}
+	else
+	{
+		Node* temp = new Node(value);
+		temp->m_pNext = m_pHead;
+		m_pHead->m_pPrev = temp;
+		m_pHead = temp;
+	}
+	m_size++;
+}
+
+template<typename T>
+void List<T>::push_back(const T& value)
+{
+	if (m_pHead == nullptr)
+	{
+		m_pHead = new Node(value);
+		m_pTail = m_pHead;
+	}
+	else
+	{
+		Node* temp = new Node(value);
+		m_pTail->m_pNext = temp;
+		temp->m_pPrev = m_pTail;
+		m_pTail = temp;
+	}
+	m_size++;
+}
+
+template<typename T>
+void List<T>::pop_front()
+{
+	if (!this->empty())
+	{
+		m_pHead = m_pHead->m_pNext;
+		m_pHead->m_pPrev = nullptr;
+		m_size--;
+	}
+}
+
+template<typename T>
+void List<T>::pop_back()
+{
+	if (!this->empty())
+	{
+		m_pTail = m_pTail->m_pPrev;
+		m_pTail->m_pNext = nullptr;
+		m_size--;
+	}
+}
+
+template<typename T>
+T& List<T>::front()
+{
+	return m_pHead->m_data;
+}
+
+template<typename T>
+T& List<T>::back()
+{
+	return m_pTail->m_data;
+}
+
+template<typename T>
+List<T>::iterator::iterator(Node*& ptr) : m_ptr(ptr)
+{
+}
+
+template<typename T>
+T& List<T>::iterator::operator*()
+{
+	return m_ptr->m_data;
+}
+
+template<typename T>
+typename List<T>::iterator& List<T>::iterator::operator++()
+{
+	m_ptr = m_ptr->m_pNext;
+	return *this;
+}
+
+template<typename T>
+typename List<T>::iterator List<T>::iterator::operator++(int)
+{
+	iterator temp = *this;
+	m_ptr = m_ptr->m_pNext;
+	return temp;
+}
+
+template<typename T>
+typename List<T>::iterator& List<T>::iterator::operator--()
+{
+	m_ptr = m_ptr->m_pPrev;
+	return *this;
+}
+
+template<typename T>
+typename List<T>::iterator List<T>::iterator::operator--(int)
+{
+	iterator temp = *this;
+	m_ptr = m_ptr->m_pPrev;
+	return temp;
+}
+
+template<typename T>
+bool List<T>::iterator::operator != (const iterator& it)
+{
+	return m_ptr != it.m_ptr;
+}
+
+template<typename T>
+typename List<T>::iterator List<T>::begin()
+{
+	iterator it(m_pHead);
+	return it;
+}
+
+template<typename T>
+typename List<T>::iterator List<T>::end()
+{
+	iterator it(m_pTail->m_pNext);
+	return it;
+}
+
+template<typename T>
+void List<T>::insert(iterator it, const T& value)
+{
+	Node* temp = new Node(value);
+
+	if (empty())
+	{
+		m_pHead = m_pTail = temp;
+		temp->m_pNext = temp->m_pPrev = nullptr;
+	}
+	else
+	{
+		if (it.m_ptr == nullptr)
+		{
+			temp->m_pNext = nullptr;
+
+			m_pTail->m_pNext = temp;
+			temp->m_pPrev = m_pTail;
+
+			m_pTail = temp;
+		}
+		else
+		{
+			if (it.m_ptr->m_pPrev == nullptr)
+			{
+				temp->m_pPrev = nullptr;
+				m_pHead = temp;
+			}
+			else
+			{
+				it.m_ptr->m_pPrev->m_pNext = temp;
+				temp->m_pPrev = it.m_ptr->m_pPrev;
+			}
+
+			temp->m_pNext = it.m_ptr;
+			it.m_ptr->m_pPrev = temp;
+		}
+	}
+
+	m_size++;
+}
+
+template<typename T>
+void List<T>::erase(iterator it)
+{
+	if (it.m_ptr != nullptr)
+	{
+		if (it.m_ptr->m_pPrev == nullptr && it.m_ptr->m_pNext == nullptr)
+		{
+			m_pHead = m_pTail = nullptr;
+		}
+
+		if (it.m_ptr->m_pPrev == nullptr && it.m_ptr->m_pNext != nullptr)
+		{
+			it.m_ptr->m_pNext->m_pPrev = nullptr;
+			m_pHead = it.m_ptr->m_pNext;
+		}
+
+		if (it.m_ptr->m_pPrev != nullptr && it.m_ptr->m_pNext == nullptr)
+		{
+			it.m_ptr->m_pPrev->m_pNext = nullptr;
+			m_pTail = it.m_ptr->m_pPrev;
+		}
+
+		if (it.m_ptr->m_pPrev != nullptr && it.m_ptr->m_pNext != nullptr)
+		{
+			it.m_ptr->m_pPrev->m_pNext = it.m_ptr->m_pNext;
+			it.m_ptr->m_pNext->m_pPrev = it.m_ptr->m_pPrev;
+		}
+
+		m_size--;
+		delete it.m_ptr;
+	}
+	else
+	{
+		cerr << "Erasing failed!" << endl;
+	}
+}
+
+template<typename T>
+void List<T>::clear()
+{
+	this->~List();
+}
+
+template<typename T>
+bool List<T>::empty()
+{
+	return !m_size;
+}
+
+#endif LIST
+
---
+++ Source.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include "List.h"  
+using namespace std;
+
+int main()
+{
+	List<int> list1;
+	list1.push_front(100);  
+	list1.push_front(200);  
+	list1.push_front(300);  
+	list1.push_back(777);
+	cout << list1.back() << endl; //777  
+	list1.pop_back();
+	cout << list1.back() << endl; //100  
+	cout << list1.front() << endl; //300  
+	list1.pop_front();
+	cout << list1.front() << endl; //200
+
+	List<int> list2;
+	list2.push_back(616);  
+	list2.push_front(515);  
+	list2.push_front(313); 
+	list2.push_back(777);
+
+	//Извежда 313 515 616 777  
+	for (List<int>::iterator it = list2.begin(); it != list2.end(); it++)  
+	{  
+		cout << *it << " ";
+	}  
+	cout << endl;
+
+	List<string> list3;  
+	list3.push_back("vidi");  
+	list3.push_back("vici");
+	List<string>::iterator iter = list3.begin();
+	list3.insert(iter, "Veni");
+
+	//Извежда Veni vidi vici 
+	for (List<string>::iterator it = list3.begin(); it != list3.end(); it++)  
+	{   
+		cout << *it << " ";  
+	}  
+	cout << endl;
+
+	List<string> list4;  
+	list4.push_back("Divide"); 
+	list4.push_back("et");  
+	list4.push_back("impera");
+
+	//Извежда Divide et impera  
+	for (List<string>::iterator it = list4.begin(); it != list4.end(); it++) 
+	{   
+		cout << *it << " ";  
+	}  
+	cout << endl;
+
+	List<string>::iterator mid = ++list4.begin();  
+	list4.erase(mid);
+
+	//Извежда Divide impera  
+	for (List<string>::iterator it = list4.begin(); it != list4.end(); it++)  
+	{   
+		cout << *it << " "; 
+	}    
+	cout << endl; 
+
+	system("pause");
+	return 0;
+}
+
